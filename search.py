59d86b6ed7d6933d5e9f58c2655cf1872e841860
CallmeCheese1 <tomiewilliams21@gmail.com>
2026-03-03 13:18:35 -0600

    Outline the search.py file

--- search.py
+++ search.py
@@ -1,2 +1,12 @@
+from environment import Grid
+from environment import Node as node
 
 #Will hold the algorithmic code for both Depth-First Search and Breadth-First Search Algorithms.
+
+def bfs(grid: Grid):
+    """Given a grid, use a Breadth-First Search algorithm to return a list of nodes pathing from the start to the goal, or return None if no path exists."""
+    pass
+
+def dfs(grid: Grid):
+    """Given a grid, use a Depth-First Search algorithm to return a list of nodes pathing from the start to the goal, or return None if no path exists."""
+    pass
